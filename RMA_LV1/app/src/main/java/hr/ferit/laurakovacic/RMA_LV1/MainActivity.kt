68af6c2024601d59abc44946f693fbe0b998962e
Laura Kovačić <laurakciic@gmail.com>
2022-03-28 15:00:50 +0200

    implementation for tasks 1 & 2

--- RMA_LV1/app/src/main/java/hr/ferit/laurakovacic/RMA_LV1/MainActivity.kt
+++ RMA_LV1/app/src/main/java/hr/ferit/laurakovacic/RMA_LV1/MainActivity.kt
@@ -3,6 +3,10 @@ package hr.ferit.laurakovacic.RMA_LV1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,7 +15,8 @@ class MainActivity : AppCompatActivity() {
 
         Log.d("MainActivity: ", "onCreate")
 
-
+        showNameAndDescription()
+        showBmi()
     }
 
     override fun onPause() {
@@ -22,5 +27,44 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         Log.d("MainActivity: ", "onDestroy")
+    }
+
+    private fun showNameAndDescription() {
+        val entryName = findViewById<EditText>(R.id.entryName)
+        val entryDescription = findViewById<EditText>(R.id.entryDescription)
+
+        val readOnlyName = findViewById<TextView>(R.id.textView3)
+        val readOnlyDescription = findViewById<TextView>(R.id.textView7)
+
+        val confirmBtn = findViewById<Button>(R.id.confirmBtn)
+
+        confirmBtn.setOnClickListener {
+            val name = entryName.text.toString()
+            readOnlyName.text = name
+
+            val description = entryDescription.text.toString()
+            readOnlyDescription.text = description
+        }
+    }
+
+    private fun showBmi(){
+        val entryHeight = findViewById<EditText>(R.id.entryHeight)
+        val entryWeight = findViewById<EditText>(R.id.entryWeight)
+        val calculateBtn = findViewById<Button>(R.id.calculateBtn)
+
+        calculateBtn.setOnClickListener {
+            val height = entryHeight.text.toString()
+            val weight = entryWeight.text.toString()
+
+            val intHeight = height.toFloat()
+            val intWeight = weight.toFloat()
+
+            val bmi = calculateBmi(intHeight, intWeight)
+            Toast.makeText(this, bmi.toString(), Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun calculateBmi(height: Float, weight: Float): Float {
+        return weight / (height * height)
     }
 }
